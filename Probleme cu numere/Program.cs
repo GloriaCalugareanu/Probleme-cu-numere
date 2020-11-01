b63e60e9068e428fd3788eb3646d8a8b123cacad
calugareanu.gloria@gmail.com <calugareanu.gloria@gmail.com>
2020-11-01 15:44:06 -0800

    primele modoficari

---
+++ Probleme cu numere/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Probleme_cu_numere
+{
+    class Program
+    {
+
+        static void ecuatieGrad1()
+        {
+            float a, b, x = 0;
+
+            a = float.Parse(Console.ReadLine());
+            b = float.Parse(Console.ReadLine());
+
+            if (a == 0) ;
+            
+                if (b == 0) ;
+            
+            {
+                Console.WriteLine("Exista o infinitate de solutii");
+            }
+            else
+            {
+                Console.WriteLine("Ecuatia nu are solutii");
+            }
+            {
+                x = -b / a;
+            }
+            Console.WriteLine("Solutia ecuatiei", x);
+        }
+                static void Main(string[] args)
+        { 
+                {
+                    ecuatieGrad1();
+                }
+            
+        }
+    }
+}
